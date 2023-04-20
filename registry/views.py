9c38557e05df41c4b687a9e3eca8b332f2ad65ba
Joh Dokler <joh.dokler@gmail.com>
2023-04-20 11:06:53 +0200

    Cleanup

--- registry/views.py
+++ registry/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import redirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.contrib.contenttypes.models import ContentType
 
