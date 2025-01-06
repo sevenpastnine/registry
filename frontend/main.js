9af62fdda55476c85f94dc1de1f572a5b41d593b
www <www@sevenpastnine.com>
2025-01-06 12:15:42 +0100

    Cleanup

--- frontend/main.js
+++ frontend/main.js
@@ -1,6 +1,6 @@
-import "htmx.org"
+import 'htmx.org'
 
-import hyperscript from "hyperscript.org"
+import hyperscript from 'hyperscript.org'
 hyperscript.browserInit()
 
 import './styles/main.sass'
@@ -8,22 +8,22 @@ import './styles/main.sass'
 // Main menu on mobile
 
 (() => {
-    const mainMenu = document.getElementById("main-menu-mobile");
-    const menuOpen = document.getElementById("main-menu-mobile-open");
-    const menuClose = document.getElementById("main-menu-mobile-close");
+    const mainMenu = document.getElementById('main-menu-mobile');
+    const menuOpen = document.getElementById('main-menu-mobile-open');
+    const menuClose = document.getElementById('main-menu-mobile-close');
 
     if (menuOpen && menuClose && mainMenu) {
-        menuOpen.addEventListener("click", function () {
-            mainMenu.classList.toggle("hidden");
-            menuOpen.classList.toggle("hidden");
-            menuClose.classList.toggle("hidden");
-            document.body.classList.toggle("overflow-hidden");
+        menuOpen.addEventListener('click', function () {
+            mainMenu.classList.toggle('hidden');
+            menuOpen.classList.toggle('hidden');
+            menuClose.classList.toggle('hidden');
+            document.body.classList.toggle('overflow-hidden');
         });
-        menuClose.addEventListener("click", function () {
-            mainMenu.classList.toggle("hidden");
-            menuOpen.classList.toggle("hidden");
-            menuClose.classList.toggle("hidden");
-            document.body.classList.toggle("overflow-hidden");
+        menuClose.addEventListener('click', function () {
+            mainMenu.classList.toggle('hidden');
+            menuOpen.classList.toggle('hidden');
+            menuClose.classList.toggle('hidden');
+            document.body.classList.toggle('overflow-hidden');
         });
     }
 })();
