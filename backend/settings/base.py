f37c4a1e197d05de8c384e6bc7555ab301b3ee91
Joh Dokler <joh.dokler@gmail.com>
2025-01-24 19:37:47 +0100

    Fix postgres full text search bug

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'markdownify.apps.MarkdownifyConfig',
 
     'django.contrib.sites',
+    'django.contrib.postgres',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
